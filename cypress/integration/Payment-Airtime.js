cca4267ffa41c1bce33d10cbdfa3db5f880cc06d
Christine Apondi Onyango <christine.onyango@ip-192-168-0-26.eu-west-1.compute.internal>
2022-04-27 11:46:04 +0300

    Combining the self and other airtime payments

--- cypress/integration/Payment-Airtime.js
+++ cypress/integration/Payment-Airtime.js
@@ -2,7 +2,7 @@ import LoginPage from "./PageObject/LoginPage";
 import APIs from "./PageObject/APIs";
 import Navigation from "./PageObject/Navigation";
 import PaymentForm from "./PageObject/PaymentForm";
-describe("Airtime Purchase to Self", function () {
+describe("Airtime Purchase to Other", function () {
   beforeEach(function () {
     cy.fixture("Login").then((Login) => {
       this.Login = Login;
@@ -12,6 +12,35 @@ describe("Airtime Purchase to Self", function () {
     });
   });
   it("Airtime Purchase to Other should be successful", function () {
+    const login = new LoginPage();
+    const navigation = new Navigation();
+    const API = new APIs();
+    const paymentForm = new PaymentForm();
+
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    });
+    login.navigate();
+    login.enterUsername(this.Login.userName);
+    login.enterPassword(this.Login.password);
+    login.submit();
+    navigation.selectSA();
+    navigation.PaymentMenu();
+    navigation.MobilePayments();
+    navigation.AirtimePayment();
+    paymentForm.AccountSelector();
+    paymentForm.AirtimeOther();
+    paymentForm.SelectBeneficiary();
+    paymentForm.PaymentAmountAirtime();
+    paymentForm.NextButton();
+    cy.server();
+    API.PaymentOrder();
+    paymentForm.SubmitPayment();
+    //paymentForm.PaymentResponse();
+    cy.wait(["@route1"], { responseTimeout: 200000 });
+    paymentForm.AssertSuccess();
+  });
+  it("Airtime Purchase to Self should be successful", function () {
     const login = new LoginPage();
     const navigation = new Navigation();
     const API = new APIs();
