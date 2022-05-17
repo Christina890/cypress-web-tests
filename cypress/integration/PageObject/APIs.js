c39b7aa2a1068a0b365c1fea633ccb405015badf
Christina890 <47111344+Christina890@users.noreply.github.com>
2022-05-17 16:45:23 +0300

    Update APIs.js

--- cypress/integration/PageObject/APIs.js
+++ cypress/integration/PageObject/APIs.js
@@ -2,43 +2,43 @@ class API {
   ProductSummary() {
     cy.route(
       "GET",
-      "/api/product-summary-presentation-service/client-api/v2/productsummary"
+      "xxxxxxxxx"
     ).as("Products");
   }
   TransactionListing() {
     cy.route(
       "GET",
-      "https://uat-dxp.imbank.com/api/transaction-presentation-service/client-api/v2/transactions?arrangementId=8a8095c979afcd7c0179afe3f1610001"
+      "xxxxxxxxx"
     ).as("Transactions");
   }
   PaymentOrder() {
     cy.route(
       "POST",
-      "/api/payment-order-service/client-api/v2/payment-orders"
+      "xxxxxxxxx"
     ).as("route1");
   }
   MpesaOrganizationQuery() {
     cy.route(
       "POST",
-      "/api/omni-integration-service/client-api/v1/mpesa-organization-query"
+      "xxxxxxxxx"
     ).as("MpesaQuery");
   }
   Eslip() {
     cy.route(
       "GET",
-      "/api/omni-integration-service/client-api/v1/eslip-query?referenceNumber=2019MSA1364105F"
+      "xxxxxxxxx"
     ).as("eslip");
   }
   BulkApproval() {
     cy.route(
       "POST",
-      "api/payment-order-service/client-api/v2/payment-orders/bulk-approvals"
+      "xxxxxxxxx"
     ).as("BulkApproval");
   }
   CardSummary() {
     cy.route(
       "GET",
-      "api/omni-integration-service/client-api/v1/card-service/cards-summary"
+      "xxxxxxxxx"
     ).as("CardSummary");
   }
 }
